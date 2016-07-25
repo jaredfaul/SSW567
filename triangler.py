1e316ae24ee7c1c032e8465fc29f7f7b777e1df2
ranman443 <Randy.McCormick@rockwellcollins.com>
2016-07-25 08:41:02 -0500

    fixed it some

--- triangler.py
+++ triangler.py
@@ -1,8 +1,23 @@
 import unittest
+#  scalene, isosceles, or equilateral,
+def classifyTriangle(a, b, c):
+    x = "Triangle sides: " + str(a) + ", " + str(b) +  ", " + str(c) +  " "
+    #check if its equilateral
+    if a == b and b == c:
+      x = x + "equilateral"
+    #check if its isoscoles
+    elif a == b or b == c:
+      x = x + "isosceles triangle"
+    else:
+      x = x + "scalene"
 
-def calculateTriangleType(side1, side2, side3):
-    print 'Triangle sides:', side1, side2, side3
+    if pow(a,2) + pow(b,2) == pow(c,2):
+        x = x + " ** RIGHT **"
+    return x
 
-    
-calculateTriangleType(1,2,3)
-calculateTriangleType(55,22,13)
+print classifyTriangle(3,4,5)
+print classifyTriangle(3,5,4)
+print classifyTriangle(1.2,1.2,3)
+print classifyTriangle(55,55,55)
+print classifyTriangle(23,13,13)
+print classifyTriangle(23,23,13)
