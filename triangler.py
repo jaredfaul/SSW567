f5dcd19d4d131c3d03db1a57d8b9353d033a6dee
ranman443 <Randy.McCormick@rockwellcollins.com>
2016-07-25 08:18:35 -0500

    first commit

---
+++ triangler.py
@@ -0,0 +1,8 @@
+import unittest
+
+def calculateTriangleType(side1, side2, side3):
+    print 'Triangle sides:', side1, side2, side3
+
+    
+calculateTriangleType(1,2,3)
+calculateTriangleType(55,22,13)
