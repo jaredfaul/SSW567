06245d39e64af73178e6da4f1ce0443b3816847b
ranman443 <Randy.McCormick@rockwellcollins.com>
2016-07-25 09:18:55 -0500

    fixed a bunch of stuff

--- triangler.py
+++ triangler.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 #  scalene, isosceles, or equilateral,
 def classifyTriangle(a, b, c):
     x = "sides: (" + str(a) + ", " + str(b) +  ", " + str(c) +  ")\t "
@@ -18,29 +19,30 @@ def classifyTriangle(a, b, c):
 class TrianglerTester(unittest.TestCase):
 
     def testRightSided(self):
+        print "\ntestRightSided"
         self.assertIn("RIGHT", classifyTriangle(3,4,5))
         self.assertIn("RIGHT", classifyTriangle(3,5,3))
         self.assertNotIn("RIGHT", classifyTriangle(3,5,5))
 
     def testIsosceles(self):
+        print "\ntestIsosceles:"
         self.assertIn("isosceles", classifyTriangle(3,5,5))
         self.assertNotIn("isosceles", classifyTriangle(3,4,5))
 
     def testEquilateral(self):
+        print "\ntestEquilateral:"
         self.assertIn("equilateral", classifyTriangle(55,55,55))
+        self.assertIn("equilateral", classifyTriangle(1.55,1.55,1.55))
         self.assertNotIn("equilateral", classifyTriangle(45,55,55))
 
     def testScalene(self):
+        print "\ntestScalene:"
         self.assertIn("scalene", classifyTriangle(3,4,5))
         self.assertNotIn("scalene", classifyTriangle(4,4,5))
 
 if __name__ == '__main__':
-    unittest.main()
-    sys.exit(0)
+    try:
+        unittest.main()
+    except SystemExit as e:
+        print ''
 
-##print classifyTriangle(3,4,5)
-##print classifyTriangle(3,5,4)
-##print classifyTriangle(1.2,1.2,3)
-##print classifyTriangle(55,55,55)
-##print classifyTriangle(23,13,13)
-##print classifyTriangle(23,23,13)
